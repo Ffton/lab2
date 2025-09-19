578a9734fc0eb082341e43c90822faea580db3ad
Ffton <babikov@sfedu.ru>
2025-09-19 08:34:37 +0300

    Create somename.py

---
+++ somename.py
@@ -0,0 +1,2 @@
+print('I had done some work here')
+print('im just creating a visibility that it is someone\'s repository so I can clone it on my pc to do properly my laboratory work so my teachers will be happy to see that someone is actually reading descriptions in the text and doing it')
